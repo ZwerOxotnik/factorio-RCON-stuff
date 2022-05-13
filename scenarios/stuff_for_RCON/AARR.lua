678462da1a69c0d873b3308d4f5dab3f1904c149
ZwerOxotnik <zweroxotnik@gmail.com>
2022-05-13 10:25:53 +0300

    Update to 0.5.2

--- scenarios/stuff_for_RCON/AARR.lua
+++ scenarios/stuff_for_RCON/AARR.lua
@@ -13,14 +13,25 @@ local is_server = false
 local print_to_rcon = rcon.print
 
 
-remote.add_interface("AARR", {
-	getSource = function()
-		print_to_rcon(script.mod_name) -- Returns "level" if it's a scenario, otherwise "AARR" as a mod.
-	end
-})
-
 function activate()
+	if is_server then return end
 	is_server = true
+	-- Active rcon stuff of other interfaces
+	for name, interface in pairs(remote.interfaces) do
+		if interface["activate_rcon"] then
+			remote.call(name, "activate_rcon")
+		end
+	end
+end
+function deactivate()
+	if not is_server then return end
+	is_server = false
+	-- Deactive rcon stuff of other interfaces
+	for name, interface in pairs(remote.interfaces) do
+		if interface["deactivate_rcon"] then
+			remote.call(name, "deactivate_rcon")
+		end
+	end
 end
 function is_activated()
 	if is_server then
@@ -29,16 +40,24 @@ function is_activated()
 		print_to_rcon('-')
 	end
 end
-function deactivate()
-	is_server = false
-end
+remote.add_interface("AARR", {
+	getSource = function()
+		local mod_name = script.mod_name
+		print_to_rcon(mod_name) -- Returns "level" if it's a scenario, otherwise "AARR" as a mod.
+		return mod_name
+	end,
+	activate = activate,
+	deactivate = deactivate,
+	is_activated = is_activated
+})
+
 
 
 ---@param name string
 function isPlayerConnected(name)
 	if not is_server then return end
 	local player = game.get_player(name)
-	if player and player.connected then
+	if player and player.valid and player.connected then
 		print_to_rcon('+')
 	else
 		print_to_rcon('-')
@@ -46,9 +65,18 @@ function isPlayerConnected(name)
 end
 
 ---@param name string
+function getPlayerIndex(name)
+	if not is_server then return end
+	local player = game.get_player(name)
+	if not (player and player.valid) then return end
+	print_to_rcon(player.index)
+end
+
+---@param name string
 function isPlayerExist(name)
 	if not is_server then return end
-	if game.get_player(name) then
+	local player = game.get_player(name)
+	if player and player.valid then
 		print_to_rcon('+')
 	else
 		print_to_rcon('-')
@@ -59,16 +87,28 @@ function getForces()
 	if not is_server then return end
 	local result = {}
 	for _, force in pairs(game.forces) do
-		result[#result+1] = force.name
+		if force.valid then
+			result[#result+1] = force.name
+		end
 	end
 	print_to_rcon(game.table_to_json(result))
+end
+
+---@param name string
+function getForceIndex(name)
+	if not is_server then return end
+	local force = game.forces[name]
+	if not (force and force.valid) then return end
+	print_to_rcon(force.index)
 end
 
 function getPlayers()
 	if not is_server then return end
 	local result = {}
 	for _, player in pairs(game.players) do
-		result[#result+1] = player.name
+		if player.valid then
+			result[#result+1] = player.name
+		end
 	end
 	if #result > 0 then
 		print_to_rcon(game.table_to_json(result))
@@ -79,7 +119,9 @@ function getConnectedPlayers()
 	if not is_server then return end
 	local result = {}
 	for _, player in pairs(game.connected_players) do
-		result[#result+1] = player.name
+		if player.valid then
+			result[#result+1] = player.name
+		end
 	end
 	if #result > 0 then
 		print_to_rcon(game.table_to_json(result))
